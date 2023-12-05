e05b9f59dccca05c01948a033678c597153f1d4b
Uper05 <jvfdessani@gmail.com>
2023-12-05 00:29:53 -0300

    parte 3

--- painel.js
+++ painel.js
@@ -1,18 +1,22 @@
 function getInfo(){
     var cell1 = document.getElementById("Ambiente");
-    var cell2 = document.getElementById("Analise");
+    var cell2 = document.getElementById("Populacao");
+    var cell3 = document.getElementById("Clima");
     var val = document.querySelector('input').value;
     var result = val.toLowerCase()
     if(result == 'terra'){
-        cell1.innerHTML = "Foda";
-        cell2.innerHTML = "Legal";
+        cell1.innerHTML = "Um local urbanizado pela Civilização Humana, possuindo diversas residências e pontos de troca ao redor do Planeta";
+        cell2.innerHTML = "Aproximadamente 10.1 Bilhões";
+        cell3.innerHTML = "O planeta apresenta uma variedade de Climas, desde frios em seus polos até quentes perto de seu centro, porém apresenta um clima majoritariament quente";
     }
     else if(result == 'marte'){
-        cell1.innerHTML = "Legal";
-        cell2.innerHTML = "Foda";    
+        cell1.innerHTML = "";
+        cell2.innerHTML = "";
+        cell3.innerHTML = "";
     }
     else{ 
         cell1.innerHTML = "ERROR:DATANOTFOUND";
         cell2.innerHTML = "ERROR:DATANOTFOUND";
+        cell3.innerHTML = "ERROR:DATANOTFOUND";
     }
 }
